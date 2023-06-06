9058d3c09fc50dd27d6d716ed1a0ebf8a7f9fd58
didwogur28 <didwogur28@naver.com>
2023-06-06 21:58:56 +0900

    feat: JDBC 프로그래밍 실습
    JDBC 프로그래밍 코드 리팩토링 및 DB 커넥션 풀 적용

--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -6,6 +6,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
